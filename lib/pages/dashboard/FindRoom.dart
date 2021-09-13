96e6d57c89fac3e80eb794e46f0d11c8101567c3
Oskar Kellermann <oskar@kellermann.team>
2021-09-13 23:56:14 +0200

    'find free rooms' feture added

--- lib/pages/dashboard/FindRoom.dart
+++ lib/pages/dashboard/FindRoom.dart
@@ -1,7 +1,11 @@
+import 'package:expandiware/models/ListItem.dart';
 import 'package:expandiware/pages/vplan/VPlan.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
 import '../vplan/VPlanAPI.dart';
+
+import '../../models/AppBar.dart';
 
 class FindRoom extends StatefulWidget {
   const FindRoom({Key? key}) : super(key: key);
@@ -12,6 +16,8 @@ class FindRoom extends StatefulWidget {
 
 class _FindRoomState extends State<FindRoom> {
   dynamic data = [];
+
+  DateTime DATE = DateTime.now();
 
   bool isNumeric(String s) {
     return double.tryParse(s) != null;
@@ -78,39 +84,91 @@ class _FindRoomState extends State<FindRoom> {
   void getData() async {
     VPlanAPI vplanAPI = new VPlanAPI();
 
-    String stringDate = vplanAPI.parseDate(DateTime.now());
-    /*Uri url = Uri.parse(
-      'https://www.stundenplan24.de/${vplanAPI.schoolnumber}/mobil/mobdaten/PlanKl$stringDate.xml',
-    );*/
+    //String stringDate = vplanAPI.parseDate(DateTime.now());
+    DateTime dateTimeDate = DateTime.parse('2021-09-09 08:30:00');
+    String stringDate =
+        vplanAPI.parseDate(DateTime.parse('2021-09-09 08:30:00'));
 
     Uri url = Uri.parse(await vplanAPI.getDayURL());
 
-    data = await vplanAPI.getVPlanJSON(url, DateTime.now());
+    data = await vplanAPI.getAllOfflineData();
+    if (data.length == 0) {
+      data = await vplanAPI.getVPlanJSON(url, DateTime.now());
+    }
 
     List<int> rooms = [];
+    for (int a = 0; a < data.length; a++) {
+      for (int i = 0; i < data[a]['data']['Klassen']['Kl'].length; i++) {
+        dynamic lessons = data[a]['data']['Klassen']['Kl'][i]['Pl']['Std'];
 
-    for (int i = 0; i < data['data']['Klassen']['Kl'].length; i++) {
-      dynamic lessons = data['data']['Klassen']['Kl'][i]['Pl']['Std'];
+        for (int j = 0; j < lessons.length; j++) {
+          String? room = lessons[j]['Ra'];
 
-      for (int j = 0; j < lessons.length; j++) {
-        String? room = lessons[j]['Ra'];
+          if (room != null && room != 'Gang') {
+            String editRoom = room
+                .replaceAll('H1', '')
+                .replaceAll('H2', '')
+                .replaceAll('H3', '')
+                .replaceAll('E', '');
+            if (int.tryParse(editRoom) != null) {
+              if (!rooms.contains(int.parse(editRoom))) {
+                rooms.add(int.parse(editRoom));
+              }
+            }
+          }
+        }
+      }
+    }
+    setState(() {
+      data = mergesort(rooms);
+    });
+    // ALL ROOMS GOT
 
-        if (room != null && room != 'Gang') {
-          String editRoom = room
-              .replaceAll('H1', '')
-              .replaceAll('H2', '')
-              .replaceAll('H3', '')
-              .replaceAll('E', '');
-          if (int.tryParse(editRoom) != null) {
-            if (!rooms.contains(int.parse(editRoom))) {
-              rooms.add(int.parse(editRoom));
+    List<int> freeRooms = [];
+
+    List<String> classes = await vplanAPI.getClassList();
+    List<dynamic> classLessons = [];
+    for (int i = 0; i < classes.length; i++) {
+      String currentClass = classes[i];
+
+      classLessons.add(
+        (await vplanAPI.getLessonsForToday(currentClass))['data'],
+      );
+    }
+
+    for (int j = 0; j < classLessons.length; j++) {
+      List<dynamic> lessons = classLessons[j];
+      for (var i = 0; i < lessons.length; i++) {
+        dynamic lesson = lessons[i];
+        if (lesson['place'] != 'null' || lesson['place'] != null) {
+          // check if lesson is in the right time
+          DateTime currentTime = DATE;
+          String hour = currentTime.hour.toString().length == 2
+              ? currentTime.hour.toString()
+              : '0' + currentTime.hour.toString();
+          String minute = currentTime.minute.toString().length == 2
+              ? currentTime.minute.toString()
+              : '0' + currentTime.minute.toString();
+
+          DateTime begin = DateTime.parse('2004-09-22 ${lesson['begin']}:00');
+          DateTime end = DateTime.parse('2004-09-22 ${lesson['end']}:00');
+
+          DateTime currentDateTime = DateTime.parse(
+            '2004-09-22 $hour:$minute:00',
+          );
+
+          if (isNumeric(lesson['place'].toString())) {
+            if (currentDateTime.isAfter(begin)) {
+              if (currentDateTime.isBefore(end)) {
+                freeRooms.add(int.parse(lesson['place']));
+              }
             }
           }
         }
       }
     }
 
-    data = mergesort(rooms);
+    data = mergesort(freeRooms);
     setState(() {});
   }
 
@@ -127,33 +185,41 @@ class _FindRoomState extends State<FindRoom> {
       child: Stack(
         children: [
           Container(
-            height: MediaQuery.of(context).size.height * 0.7,
-            alignment: Alignment.center,
-            child: ListView(
-              shrinkWrap: true,
-              children: [
-                Text(
-                  'all rooms:',
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-                ...data.map(
-                  (e) => Container(
-                    child: Text('$e'),
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: MediaQuery.of(context).size.height * 0.9,
+              child: GridView.count(
+                crossAxisCount: 3,
+                childAspectRatio: 3 / 2,
+                shrinkWrap: true,
+                physics: BouncingScrollPhysics(),
+                children: [
+                  ...data.map(
+                    (e) => Container(
+                      padding: EdgeInsets.all(8),
+                      margin: EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Theme.of(context).backgroundColor,
+                      ),
+                      child: Center(
+                        child: Text(
+                          '$e',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ),
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
-          Container(
-            alignment: Alignment.topLeft,
-            margin: EdgeInsets.only(top: 30, left: 10),
-            child: IconButton(
-              icon: Icon(
-                Icons.arrow_back_ios_rounded,
-                color: Theme.of(context).focusColor,
-              ),
-              onPressed: () => Navigator.pop(context),
-            ),
+          Appbar(
+            'Raum finden',
+            SizedBox(),
           ),
         ],
       ),
